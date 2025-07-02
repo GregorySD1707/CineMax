104fb4ea4b75ce9a2b41f06943c2eaca2add1b83
mateonicolasg <mateo.simbana@epn.edu.ec>
2025-07-01 19:22:43 -0500

    Añadiendo getter y setter y cambiando el constructor de la clase Cliente

--- Modelos/VentaDeBoletos/Cliente.java
+++ Modelos/VentaDeBoletos/Cliente.java
@@ -1,15 +1,47 @@
 package Modelos.VentaDeBoletos;
 
 public class Cliente {
+    private String cedula;
     private String nombre;
     private String apellido;
-    private String cedula;
     private String correoElectronico;
 
-    public Cliente(String nombre, String apellido, String cedula, String correoElectronico) {
+    public Cliente(String cedula, String nombre, String apellido, String correoElectronico) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.cedula = cedula;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
         this.correoElectronico = correoElectronico;
     }
 
