1fe98d65f7476c123390bdb8a85996bc9ba953bd
mateonicolasg <mateo.simbana@epn.edu.ec>
2025-07-01 19:29:39 -0500

    Probando la conexión con PostgreSQL mediante la creación de un Cliente

--- Main.java
+++ Main.java
@@ -1,16 +1,28 @@
+import DAOs.VentaDeBoletos.ClienteDAO;
 import Modelos.CineMax;
 import Controladores.ControladorCineMax;
+import Modelos.VentaDeBoletos.Cliente;
 import Vistas.VistaCineMax;
 
 public class Main{
-    public static void main(String[] args) {
-        VistaCineMax vistaCineMax = new VistaCineMax();
+    public static void main(String[] args) throws Exception{
+        //VistaCineMax vistaCineMax = new VistaCineMax();
 
-        CineMax cineMax = new CineMax();
+        //CineMax cineMax = new CineMax();
         
-        ControladorCineMax controladorCineMax = new ControladorCineMax(cineMax, vistaCineMax);
+        //ControladorCineMax controladorCineMax = new ControladorCineMax(cineMax, vistaCineMax);
         
-        controladorCineMax.mostrarPaginaPrincipal();
+        //controladorCineMax.mostrarPaginaPrincipal();
         //cineMax.iniciar();
+
+        ClienteDAO clienteDAO = new ClienteDAO();
+
+        Cliente cliente = new Cliente("1234567890", "Juan", "Pérez", "juan.perez@epn.edu.ec");
+
+        clienteDAO.create(cliente);
+
+        Cliente cliente2 = clienteDAO.readBy("1234567890");
+
+        System.out.println(cliente2.toString());
     }
 }
